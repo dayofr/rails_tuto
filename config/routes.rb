2e6311e63b4dc098e06ff758c3283010ab018425
Dayo <dayo@dayo.fr>
2013-09-12 20:57:28 +0200

    Add post autogenerated file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RailsTuto::Application.routes.draw do
+  resources :posts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
